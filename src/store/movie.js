c4605d58d63cb0a573380946f912f4e4ec0b400a
SoJaeBeom <sojb0826@gmail.com>
2022-02-18 22:42:08 +0900

    Add Movie Search

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,27 +1,40 @@
+import axios from 'axios'
+
 export default {
   // module
   namespaced: true,
   // data
   state: () => ({
-    movies: []
+    movies: [],
+    message: '',
+    loading: false
   }),
   // computed
-  getters: {
-    movieIds(state) {
-      return state.movies.map(m => m.imdbID)
-    }
-  },
+  getters: {},
   // methods
   // 변이 -> state(데이터)를 변경, 여기서만 데이터를 변경할 수 있다.
   mutations: {
+    updateState(state, payload) {
+      // ['movies', 'message', 'loading']
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     resetMovies(state) {
       state.movies = []
     }
   },
   // 비동기로 처리
   actions: {
-    searchMovies() {
-      
+    async searchMovies({ commit }, payload) {
+      const { title, type, number, year } = payload
+      const OMDB_API_KEY = '7035c60c'
+
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const { Search, totalResults } = res.data
+      commit('updateState', {
+        movies: Search
+      })
     }
   }
 }
