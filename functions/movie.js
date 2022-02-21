9adb2abfa1bb70694e4f92c811c22092d7ace059
SoJaeBeom <sojb0826@gmail.com>
2022-02-21 17:04:47 +0900

    Add local and server environment variables configuration

--- functions/movie.js
+++ functions/movie.js
@@ -1,11 +1,11 @@
 // 서버리스함수는 nodejs 환경으로 동작한다.
 const axios = require('axios')
+const { OMDB_API_KEY } = process.env
 
 exports.handler = async function (event) {
   console.log(event)
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
   const url = id 
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
