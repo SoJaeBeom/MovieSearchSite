ee1b2ae24ef453b2773865b71a818fc09c231682
SoJaeBeom <sojb0826@gmail.com>
2022-02-21 10:53:02 +0900

    Add Vue plugin (image load event)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js' // index.js파일을 참조하는 거라면 생략 가능하다.
 import store from './store/index.js'
+import loadImage from './plugins/loadImage'
 
 createApp(App)
-  .use(router) // .use() : Vue.js에 연결할 플러그인, 라이브러리를 지정하는 용도로 사용
-  .use(store)
+  .use(router) // .use() : Vue.js에 연결할 플러그인, 라이브러리를 지정하는 용도로 사용... $route, $router
+  .use(store) // $store
+  .use(loadImage) // $loadImage
   .mount('#app')
